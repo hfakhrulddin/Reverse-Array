d572104e31cdc7783ce393c04bf5ace72d82bc79
Hussen Fakhrulddin <hf556482@gmail.com>
2016-04-02 14:50:13 -0700

    init

---
+++ Reverse String/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Reverse_String
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            bool Exit = false;
+            while (Exit ==  false)
+                {
+                
+                Console.Write("Enter a string or press e to EXIT" + "\r\n");
+
+                string InPut = Console.ReadLine();
+                char[] process = InPut.ToCharArray();
+                Array.Reverse(process);
+                Console.WriteLine(process);
+
+                if (InPut == "e")
+                {
+                Exit = true;
+                }
+            }
+
+        }
+    }
+}
